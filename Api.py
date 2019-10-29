a088a98e443f3cbfd617c31b82e88451cb6cfa38
Florian <florianelie8@gmail.com>
2019-10-29 16:41:19 +0100

    update API class to call complete url request.

--- Api.py
+++ Api.py
@@ -6,10 +6,14 @@ import json
 class Api:
 
     # Exemple d'apel à l'API : bulbasaur = Api.callApi("pokemon/1/")
-
+    # PS : Vous pouvez maintenant mettre une url complète comprenant "http" ou continuer à mettre le endpoint.
+    # l'URL complète peut-être utile dans le cas ou vous récupérez l'URL complète dans un objet
     @staticmethod  # rendre la méthode static
     def callApi(endpoint):
-        baseURI = "https://pokeapi.co/api/v2/"
+        if 'http' in endpoint:
+            baseURI = ''
+        else:
+            baseURI = "https://pokeapi.co/api/v2/"
         try:
             response = requests.get(baseURI + endpoint)
         except HTTPError as httpErr:
