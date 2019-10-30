02d30ebc82a90afbfc3a875289735b0aea17c1aa
Guerin Yves <yves_guerin@groupevsc.com>
2019-10-30 16:21:45 +0100

    enemy

---
+++ enemy.py
@@ -0,0 +1,19 @@
+from player import Player
+from pokedex import pokedex
+from pokemon import Pokemon
+from random import *
+²
+class Enemy:
+
+    def __init__(self, name):
+        self.player = Player(name)
+        numberPokemon = randint(2,4)
+        pokedex.getPokemon(50)
+        for i in range(numberPokemon):
+            self.player.addPokemon(pokedex.listePokemon[randint(0,49)])
+    
+    def displayEnemy(self):
+        print("------ Enemy - "+ str(self.player.getName()) +"------")
+        for pokemon in self.player.getPokemon():
+            print(str(i)+"-"+pokemon.name)
+            i+=1
